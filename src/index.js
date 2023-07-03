2c059cb02c9380dbf9cf2884a366dad26d942efe
Sean <soxfansean@gmail.com>
2023-07-02 20:16:50 -0400

    Finish unstyled version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { format, compareAsc } from 'date-fns';
+import { formatRelative } from 'date-fns';
 
 const addProjectButton = document.querySelector("#add-project");
 const submitProjectButton = document.querySelector(".submit-project");
@@ -20,16 +20,10 @@ function Todo(title, description, dueDate){
 }
 
 function TodoList(){
-    const list = [];
-
-    const sortTodo = () => {
-        //Need to have it actually for the list of 
-        //list = list.sort((left, right) => compareAsc(left.getDueDate, right.getDueDate));
-    };
+    let list = [];
 
     const addTodo = todo => {
         list.push(todo);
-        sortTodo();
     };
 
     const removeTodo = index => {
@@ -58,13 +52,9 @@ function ProjectList(){
         list.push(project);
     };
 
-    const removeProject = index => {
-        list.splice(index, 1);
-    };
-
     const getList = () => list;
 
-    return {addProject, getList, removeProject};
+    return {addProject, getList};
 }
 
 function updateProjectButtons(){
@@ -85,7 +75,99 @@ function updateProjectButtons(){
 function displayProject(project){
     const content = document.querySelector(".content");
     content.innerHTML = '';
-    content.textContent = project.getName();
+    const header = document.createElement("div");
+    header.classList.add("todo-header");
+    const projectTitle = document.createElement("h2");
+    projectTitle.id = "project-title";
+    projectTitle.textContent = project.getName();
+    header.appendChild(projectTitle);
+    const newTaskButton = document.createElement("button");
+    newTaskButton.id = "new-task-button";
+    newTaskButton.textContent = "New Task:";
+    const newTaskForm = document.createElement("div");
+    newTaskForm.classList.add("hidden-form");
+    newTaskButton.addEventListener("click", () => {
+        newTaskForm.classList.toggle("revealed-form");
+    });
+    const nameLabel = document.createElement("label");
+    nameLabel.for = "name";
+    nameLabel.textContent = "Name:";
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.id = "name";
+    nameInput.name = "name";
+    const desLabel = document.createElement("label");
+    desLabel.for = "description";
+    desLabel.textContent = "Description:";
+    const desInput = document.createElement("textarea");
+    desInput.id = "description";
+    desInput.name = "description";
+    const dateLabel = document.createElement("label");
+    dateLabel.for = "date";
+    dateLabel.textContent = "date";
+    const dateInput = document.createElement("input");
+    dateInput.type = "datetime-local";
+    dateInput.id = "date";
+    dateInput.name = "date";
+    const submitTaskButton = document.createElement("button");
+    submitTaskButton.id = "submit-task";
+    submitTaskButton.textContent = "Enter";
+    submitTaskButton.addEventListener("click", () => {
+        createTask(project.getList());
+        displayProject(project);
+    });
+    newTaskForm.appendChild(nameLabel);
+    newTaskForm.appendChild(nameInput);
+    newTaskForm.appendChild(desLabel);
+    newTaskForm.appendChild(desInput);
+    newTaskForm.appendChild(dateLabel);
+    newTaskForm.appendChild(dateInput);
+    newTaskForm.appendChild(submitTaskButton);
+    header.appendChild(newTaskButton);
+    header.appendChild(newTaskForm);
+    content.appendChild(header);
+
+    content.appendChild(renderTasks(project.getList(), project));
+}
+
+function createTask(taskList){
+    const name = document.querySelector("#name").value;
+    const description = document.querySelector("#description").value;
+    const date = new Date(document.querySelector("#date").value);
+    const task = Todo(name, description, date);
+    taskList.addTodo(task);
+}
+
+function renderTasks(taskList, project){
+    const wrapper = document.createElement("div");
+    for(let i = 0; i < taskList.getList().length; i++){
+        wrapper.appendChild(createDOMTask(taskList.getList()[i], i, project));
+    }
+    return wrapper;
+}
+
+function createDOMTask(task, index, project){
+    const taskElement = document.createElement("div");
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = task.getTitle();
+    taskElement.appendChild(titleElement);
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = task.getDescription();
+    taskElement.appendChild(descriptionElement);
+    const dateElement = document.createElement("h4");
+    dateElement.textContent = `Due ${formatRelative(task.getDueDate(), new Date)}`;
+    taskElement.appendChild(dateElement);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete");
+    deleteButton.dataset.index = index;
+    deleteButton.addEventListener("click", () => {
+        project.getList().removeTodo(deleteButton.dataset.index);
+        displayProject(project);
+    });
+    taskElement.appendChild(deleteButton);
+    return taskElement;
+
 }
 
 addProjectButton.addEventListener("click", () => {
